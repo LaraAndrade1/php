914ff09e806954f463f0b3088a09696cb0d1dcde
LaraAndrade1 <laraandrade0597@gmail.com>
2023-08-10 13:32:53 -0300

    Update sintaxe3.php

--- Aula01/sintaxe3.php
+++ Aula01/sintaxe3.php
@@ -1,8 +1,9 @@
 <?php
 
-echo               "testando espaçoem branco <br>";
+echo               "testando espaço em branco <br>";
 echo "testando espaço <br>"
 ;
+
 
 echo "quebra de linha <br>"
 ;
